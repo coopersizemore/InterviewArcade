9764d3cb862a57e6d392d0f3c728da50f94db135
EverettTucker471 <tucker24e@ncssm.edu>
2025-10-11 16:19:17 -0400

    Adding models

--- python-backend/models.py
+++ python-backend/models.py
@@ -16,3 +16,46 @@ class InterviewQuestion(BaseModel):
     starterCode: Dict[str, str]
     hints: List[str]
     companies: Optional[List[str]] = []
+
+
+class Company(BaseModel):
+    """
+    Stores information about a company
+    """
+    id: int
+    name: str
+    description: Optional[str] = ""
+
+
+class FeedbackResponse(BaseModel):
+    """
+    Stores information about a feedback response from the LLM
+    This is incomplete, edit for specific use case
+    """
+    scoring: str
+    feedback: str
+    transcript: str
+
+
+class FeedbackRequest(BaseModel):
+    """
+    Stores information about the feedback request
+    This doesn't include 
+    """
+    code: str
+    question: str
+    audio_filepath: str
+
+
+class TTSRequest(BaseModel):
+    """
+    Stores information aboyt a text-to-speech request
+    """
+    transcript: str
+
+
+class TTSResponse(BaseModel):
+    """
+    Returns the text-to-speech audio file produced by eleven labs
+    """
+    audio_filepath: str
