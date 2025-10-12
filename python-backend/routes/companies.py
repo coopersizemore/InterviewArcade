ccf5399e65e91d6a99ac2d5f33e4f60511efe382
EverettTucker471 <tucker24e@ncssm.edu>
2025-10-11 22:40:32 -0400

    Updating company routes

--- python-backend/routes/companies.py
+++ python-backend/routes/companies.py
@@ -1,13 +1,22 @@
 from fastapi import APIRouter
 from services.questions_service import load_questions_from_file
+from models import Company
 
 router = APIRouter(prefix="/api/companies", tags=["companies"])
 
-@router.get("/", response_model=list[str])
+@router.get("/", response_model=list[Company])
 async def get_companies():
     """Endpoint to get a list of all companies"""
     companies = set()
     for q in load_questions_from_file():
         for company in q["companies"]:
             companies.add(company)
+    rtn = []
+    companies = list(companies)
+    for i in range(len(companies)):
+        rtn.append(Company(
+            id=i,
+            name=companies[i],
+            description=""
+        ))
     return list(companies)
